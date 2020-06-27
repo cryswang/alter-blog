179299f5430014494203a5483411b2f6749918c4
Crystal <cryswang@terpmail.umd.edu>
2020-06-27 16:46:38 -0400

    updated post model in views.py and added experience model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Experience(models.Model):
+    company = models.CharField(max_length=200, default="company")
+    title = models.CharField(max_length=200, default="Intern")
+    description =  models.TextField(default="desc")
+    location = models.CharField(max_length=200, default="place, place")
+    work_period = models.CharField(max_length=200, default="date - date")
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.company
