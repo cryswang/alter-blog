1d898804abe8b8404546fdb46fc7bf991bc7eac1
Crystal <cryswang@terpmail.umd.edu>
2020-07-25 14:41:00 -0400

    added initial tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+# from selenium import webdriver
 
-# Create your tests here.
+# browser = webdriver.Chrome()
+# browser.get('http://localhost:8000')
+
+# assert 'Django' in browser.title
